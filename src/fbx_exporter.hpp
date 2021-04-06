e102aff9c74a196f7e0f613ecb5f4bdfe63ce20a
Cracklings <Cracklings3D@gmail.com>
2021-04-06 09:20:18 +0800

    add normal to vertex

--- src/fbx_exporter.hpp
+++ src/fbx_exporter.hpp
@@ -6,6 +6,7 @@
 #include <fbxsdk.h>
 
 #include <cstdio>
+#include <fbxsdk/scene/geometry/fbxlayer.h>
 #include <gsl/gsl>
 #include <unordered_map>
 
@@ -15,21 +16,25 @@ class Fbx_exporter {
 public:
   template <typename Elements>
   static bool export_to_file(const std::string &path, const poly<Elements> &p) {
-    FbxManager *fbx_manager = FbxManager::Create();
+    FbxManager *manager = FbxManager::Create();
     // FbxIOSettings *io_settings = FbxIOSettings::Create(fbx_manager, IOSROOT);
     // fbx_manager->SetIOSettings(io_settings);
 
-    FbxExporter *fbx_exporter = FbxExporter::Create(fbx_manager, path.c_str());
-    auto exporter_init_result = fbx_exporter->Initialize(path.c_str());
+    FbxExporter *exporter = FbxExporter::Create(manager, path.c_str());
+    auto exporter_init_result = exporter->Initialize(path.c_str());
     Ensures(exporter_init_result);
 
-    FbxScene *scene = FbxScene::Create(fbx_manager, "scene");
+    FbxScene *scene = FbxScene::Create(manager, "scene");
     FbxNode *root_node = scene->GetRootNode();
 
-    FbxNode *mesh_node = FbxNode::Create(fbx_manager, "mesh node");
-    FbxMesh *mesh = FbxMesh::Create(fbx_manager, "mesh");
+    FbxNode *mesh_node = FbxNode::Create(manager, "mesh node");
+    FbxMesh *mesh = FbxMesh::Create(manager, "mesh");
     mesh_node->AddNodeAttribute(mesh);
     root_node->AddChild(mesh_node);
+
+    FbxLayerElementNormal *normal =
+        FbxLayerElementNormal::Create(mesh, "normal");
+    normal->SetMappingMode(FbxLayerElement::EMappingMode::eByControlPoint);
 
     std::unordered_map<cmg::ci_v<Elements>, size_t> hds_vertex_index_map;
     int i = 0;
@@ -40,6 +45,10 @@ public:
       mesh->SetControlPointAt(fbx_point, i);
       printf("Vertex #%d: (%f, %f, %f)\n", i, v_it->point().x(),
              v_it->point().y(), v_it->point().z());
+      normal->GetDirectArray().SetAt(
+          i, FbxVector4(v_it->Normal.x(), v_it->Normal.y(), v_it->Normal.z()));
+      printf("Normal #%d: (%f, %f, %f)\n", i, v_it->Normal.x(),
+             v_it->Normal.y(), v_it->Normal.z());
       i++;
     }
 
@@ -50,17 +59,18 @@ public:
       mesh->BeginPolygon();
       printf("Face #%d: ", j);
 
-      auto v_it = f_it->halfedge();
+      auto h_it = f_it->halfedge();
       do {
-        mesh->AddPolygon(hds_vertex_index_map[v_it->vertex()]);
-        printf("%d ", j);
-        j++;
-      } while (v_it != f_it->halfedge());
+        mesh->AddPolygon(hds_vertex_index_map[h_it->vertex()]);
+        printf("%ld ", hds_vertex_index_map[h_it->vertex()]);
+        h_it = h_it->next();
+      } while (h_it != f_it->halfedge());
       mesh->EndPolygon();
+      j++;
       printf("\n");
     }
 
-    return fbx_exporter->Export(scene);
+    return exporter->Export(scene);
   }
 
   template <typename Elements>
--- src/mesh.hpp
+++ src/mesh.hpp
@@ -1,42 +1,56 @@
 #pragma once
 
 #include "polyhedron.hpp"
+#include <CGAL/HalfedgeDS_face_base.h>
 #include <CGAL/HalfedgeDS_vertex_base.h>
 #include <CGAL/Polyhedron_items_3.h>
 #include <CGAL/Vector_3.h>
 #include <CGAL/tags.h>
 
 namespace cmg {
-template <class R, typename Tag, class Point>
-class Vertex_with_normal : public CGAL::HalfedgeDS_vertex_base<R, Tag, Point> {
-  CGAL::Vector_3<k> Normal = {0, 1, 0};
+template <class R, class T, class P, class Tr>
+class Vertex_with_normal : public CGAL::HalfedgeDS_vertex_base<R, T, P> {
+public:
+  using Point_3 = typename Tr::Point_3;
+
+  typename Tr::Vector_3 Normal = typename Tr::Vector_3(0, 1, 0);
+
+  Vertex_with_normal(const Point_3 &point)
+      : CGAL::HalfedgeDS_vertex_base<R, T, P>(point) {
+    CGAL::HalfedgeDS_vertex_base<R, T, P>();
+  }
+  Vertex_with_normal() {}
+};
+
+template <class R>
+class Face_with_color : public CGAL::HalfedgeDS_face_base<R> {
+  CGAL::Color color;
 };
 
 class Mesh_elements_default : public CGAL::Polyhedron_items_3 {
-  // TODO: problem is definitely in here.
-  // TODO: problem is definitely in here.
-  // TODO: problem is definitely in here.
-  // TODO: problem is definitely in here.
 public:
   template <class R, class Tr> struct Vertex_wrapper {
     using Point = typename Tr::Point_3;
-    using Vertex = Vertex_with_normal<R, CGAL::Tag_true, Point>;
+    using Vertex = Vertex_with_normal<R, CGAL::Tag_true, Point, Tr>;
+  };
+  template <class R, class Tr> struct Face_wrapper {
+    using Face = Face_with_color<R>;
   };
 };
 
-template<typename Elements>
-class Mesh {
+template <typename Elements> class Mesh {
 private:
-  poly<Elements> p;
+  using poly_T = poly<Elements>;
+  poly_T p;
 
 public:
   Mesh() {
-    k::Point_3 pt0(1, 0, 0);
-    k::Point_3 pt1(0, 1, 0);
-    k::Point_3 pt2(0, 0, 1);
-    k::Point_3 pt3(0, 0, 0);
-    
-    p.make_tetrahedron(pt0, pt1, pt2, pt3);
+    typename poly_T::Point pt0(1, 0, 0);
+    typename poly_T::Point pt1(0, 1, 0);
+    typename poly_T::Point pt2(0, 0, 1);
+    typename poly_T::Point pt3(0, 0, 0);
+
+    auto t = p.make_tetrahedron(pt0, pt1, pt2, pt3);
   }
   friend class Fbx_exporter;
 };
